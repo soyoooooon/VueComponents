820d85b66336249ee66d244e7df9751a30209365
이소윤 <s_lee77176@fanshaweonline.ca>
2020-02-04 16:49:55 -0500

    simple inital build

---
+++ js/main.js
@@ -0,0 +1,39 @@
+//create a couple of components we cana request and render
+let SplashComponent = {
+    template: `<h1>Welcome to my Super Awesome Soyoon's App!</h1>`
+};
+
+let AppComponent = {
+    template: `<h1>This is the App Page</h1>`
+
+};
+
+let ErrorComponent = {
+    template: `<h1 class="error">This page doesn't exist!</h1>`
+};
+
+//these are the same as Express routes -> router.get('/', ... do smth with the request)
+const routes = [
+    { path: '/', name:'splash', component: SplashComponent },
+    { path: '/app', name:'app', component: AppComponent },
+    { path: '*', name: 'error', component: ErrorComponent }
+]
+
+const router = new VueRouter({
+    routes // short for `routes: routes`
+  })
+
+
+const vm = new Vue({
+    data: {
+
+    },
+
+    method: {
+
+    },
+
+    router
+
+
+}).$mount("#app");
